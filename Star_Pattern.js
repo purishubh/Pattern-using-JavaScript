2f47d64c76fa4cd74d24c10251aff0f758887395
Shubhendra Puri <purishubh@gmail.com>
2022-02-09 20:55:22 +0530

    Added comment
    Added comment to understand each step

--- Star_Pattern.js
+++ Star_Pattern.js
@@ -1,18 +1,18 @@
 console.log("Welcome to Star patter using JavaScript demo");
 
-var numb = 5; 
-var str = ''; 
+var numb = 5; // variable initialize for number of rows
+var str = ''; // blank variable define to store pattern
 
-for(var i=1; i<=numb; i++){ 
+for(var i=1; i<=numb; i++){ // external loop to print row
   
-  for(var space=1; space<=numb-i; space++){ 
-    str += ' '; 
+  for(var space=1; space<=numb-i; space++){ // internal loop for leave space
+    str += ' '; // print blank space
   }
   
-  for(var j=1; j<=2*i-1; j++){ 
-    str += '*';  
+  for(var j=1; j<=2*i-1; j++){ // internal loop for star
+    str += '*'; // print star (*) 
   }
-  str += '\n' 
+  str += '\n' // new line
 }
 
-console.log(str); 
+console.log(str); // console the output of star pattern
